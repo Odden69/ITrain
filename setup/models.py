3946782839db5f74b56dee9424cbc53e7ead99f0
Odden69 <helena.joh2@gmail.com>
2022-04-04 19:17:51 +0000

    feat: Add unit models to the setup app, closes #21

--- setup/models.py
+++ setup/models.py
@@ -1,3 +1,31 @@
+"""
+This module specifies the models which belong to the setup app.
+These models are used as building blocks in the main app.
+"""
 from django.db import models
 
-# Create your models here.
+
+class ExerciseUnit(models.Model):
+    """
+    ExerciseUnits are created by admin and used in Exercise Models
+    """
+    name = models.CharField(max_length=5, unique=True, null=False, blank=False)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class RepsUnit(models.Model):
+    """
+    RepsUnits are created by admin and used in Workout Models
+    """
+    name = models.CharField(max_length=5, unique=True, null=False, blank=False)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
