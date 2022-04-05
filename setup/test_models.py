663faa967f272f9dc7a51549309e08d520e80b3c
Odden69 <helena.joh2@gmail.com>
2022-04-05 20:37:38 +0000

    test: ExerciseUnit and RepsUnit, closes #24

---
+++ setup/test_models.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from .models import ExerciseUnit, RepsUnit
+
+class TestSetup (TestCase):
+
+    def test_exercise_unit_string_method_returns_name(self):
+        exercicse_unit = ExerciseUnit.objects.create(name='TstEU')
+        self.assertEqual(str(exercicse_unit), 'TstEU')
+
+    def test_reps_unit_string_method_returns_name(self):
+        reps_unit = RepsUnit.objects.create(name='TstRU')
+        self.assertEqual(str(reps_unit), 'TstRU')
