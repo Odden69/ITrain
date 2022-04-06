c5e03007606ad69026fcd7490b40c0d4eb77c363
Odden69 <helena.joh2@gmail.com>
2022-04-06 06:39:20 +0000

    fix: Correct spelling in TestSetup

--- setup/test_models.py
+++ setup/test_models.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 from .models import ExerciseUnit, RepsUnit
 
-class TestSetup (TestCase):
+class TestSetup(TestCase):
 
     def test_exercise_unit_string_method_returns_name(self):
-        exercicse_unit = ExerciseUnit.objects.create(name='TstEU')
-        self.assertEqual(str(exercicse_unit), 'TstEU')
+        exercise_unit = ExerciseUnit.objects.create(name='TstEU')
+        self.assertEqual(str(exercise_unit), 'TstEU')
 
     def test_reps_unit_string_method_returns_name(self):
         reps_unit = RepsUnit.objects.create(name='TstRU')
