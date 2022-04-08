1c56542751645db75b25daf808f15aefc6db5968
Odden69 <helena.joh2@gmail.com>
2022-04-08 19:07:59 +0000

    feat: Add exercise model to admin.py
    An empty string had to be added to exercise_unit and
    therefore the option Blank was changed to True

--- setup/admin.py
+++ setup/admin.py
@@ -2,9 +2,9 @@
 This module adjusts the admin model, which is predefined by django
 """
 from django.contrib import admin
-from .models import ExerciseUnit, RepsUnit
+from .models import ExerciseUnit, RepsUnit, Exercise
 
 
-adminFields = [ExerciseUnit, RepsUnit]
+adminFields = [ExerciseUnit, RepsUnit, Exercise]
 
 admin.site.register(adminFields)
--- setup/models.py
+++ setup/models.py
@@ -9,7 +9,7 @@ class ExerciseUnit(models.Model):
     """
     ExerciseUnits are created by admin and used in Exercise Models
     """
-    name = models.CharField(max_length=5, unique=True, null=False, blank=False)
+    name = models.CharField(max_length=5, unique=True, null=False, blank=True)
 
     class Meta:
         ordering = ['name']
@@ -23,6 +23,21 @@ class RepsUnit(models.Model):
     RepsUnits are created by admin and used in Workout Models
     """
     name = models.CharField(max_length=5, unique=True, null=False, blank=False)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Exercise(models.Model):
+    """
+    Exercises can be created by authorized users. Are used in Workouts.
+    """
+    name = models.CharField(max_length=30, unique=True, null=False, blank=False)
+    unit = models.ForeignKey(ExerciseUnit, on_delete=models.SET_DEFAULT, blank=True, default="")
+    description = models.TextField(max_length=200)
 
     class Meta:
         ordering = ['name']
