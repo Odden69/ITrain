61ceb7b837e48337b631d67ced0d23ad76c6031e
Odden69 <helena.joh2@gmail.com>
2022-05-05 16:05:05 +0000

    setup: Add database selection to settings.py

--- itrain/settings.py
+++ itrain/settings.py
@@ -95,16 +95,17 @@ WSGI_APPLICATION = 'itrain.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.path.isfile("env.py"):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation
