0ac6b028f427c5c80142c7f7da235dce7299ba52
Odden69 <helena.joh2@gmail.com>
2022-05-14 19:45:21 +0000

    style: Adjust style on exercise form, closes #44
    Minor changes to the exercise form to correlate to the other forms.

--- setup/forms.py
+++ setup/forms.py
@@ -1,17 +1,34 @@
+
 """
 This module contains forms used in the setup app.
 """
 from django import forms
-from .models import Exercise
+from .models import Exercise, ExerciseUnit
 
 
 class ExerciseForm(forms.ModelForm):
     """
     Form for editing or creating Exercises
     """
+    unit = forms.ModelChoiceField(queryset=ExerciseUnit.objects.all(),
+                                  empty_label='Select a Unit, - for none')
+
     class Meta:
         model = Exercise
         fields = ['name',
                   'unit',
                   'description',
                   ]
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter Exercise Name',
+                    }
+                    ),
+            'description': forms.Textarea(
+                attrs={
+                    'rows': 4,
+                    'placeholder': 'Enter a description of the exercise',
+                    }
+                    ),
+        }
