8ce5a9f87ad3ebc51f33248d61a4d6667cebbb31
Odden69 <helena.joh2@gmail.com>
2022-05-19 20:24:54 +0000

    style: Fix navbar, closes #57
    The active page is now marked, apart from the home page at start up.
    I'll see if I can figure out how to solve that later.

--- setup/urls.py
+++ setup/urls.py
@@ -5,17 +5,19 @@ from django.urls import path
 
 
 urlpatterns = [
-     path('exercises/', ExerciseList.as_view(), name='exercises'),
-     path('edit_exercise/<int:exercise_id>/', edit_exercise,
+     path('settings/exercises/', ExerciseList.as_view(), name='exercises'),
+     path('settings/edit_exercise/<int:exercise_id>/', edit_exercise,
           name='edit_exercise'),
-     path('create_exercise/', create_exercise, name='create_exercise'),
-     path('delete_exercise/<int:exercise_id>/', delete_exercise,
+     path('settings/create_exercise/', create_exercise,
+          name='create_exercise'),
+     path('settings/delete_exercise/<int:exercise_id>/', delete_exercise,
           name='delete_exercise'),
-     path('muscle_groups/', MuscleGroupList.as_view(), name='muscle_groups'),
-     path('edit_muscle_group/<int:muscle_group_id>/', edit_muscle_group,
-          name='edit_muscle_group'),
-     path('create_muscle_group/', create_muscle_group,
+     path('settings/muscle_groups/', MuscleGroupList.as_view(),
+          name='muscle_groups'),
+     path('settings/edit_muscle_group/<int:muscle_group_id>/',
+          edit_muscle_group, name='edit_muscle_group'),
+     path('settings/create_muscle_group/', create_muscle_group,
           name='create_muscle_group'),
-     path('delete_muscle_group/<int:muscle_group_id>/', delete_muscle_group,
-          name='delete_muscle_group')
+     path('settings/delete_muscle_group/<int:muscle_group_id>/',
+          delete_muscle_group, name='delete_muscle_group')
 ]
